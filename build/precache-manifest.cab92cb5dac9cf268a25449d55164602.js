556b7e1149c4a986d39bd6b7997f23a8698f81b5
Huit Carlos <emox2544@gmail.com>
2019-05-24 03:14:10 -0600

    fix paths

---
+++ build/precache-manifest.cab92cb5dac9cf268a25449d55164602.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c52b625af42b27bd58c037349b2c0900",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "847ad6b2eef74ef3b272",
+    "url": "/website/static/css/main.9c4e72a7.chunk.css"
+  },
+  {
+    "revision": "5053afa1974bd8aebd0a",
+    "url": "/website/static/js/2.9a3f3bc0.chunk.js"
+  },
+  {
+    "revision": "847ad6b2eef74ef3b272",
+    "url": "/website/static/js/main.bb2ab425.chunk.js"
+  },
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.de462641.js"
+  },
+  {
+    "revision": "3a5cb802b3c8678071e22a08f581ab59",
+    "url": "/website/static/media/background.3a5cb802.jpg"
+  },
+  {
+    "revision": "140bacecd396ddc67046c7a5c46f78d1",
+    "url": "/website/static/media/profile.140bacec.jpg"
+  }
+]);
